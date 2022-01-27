2571232abb269a1ea4106ce0e9457cc601a20a16
huangwenwu <huangww@sobot.com>
2022-01-27 11:51:31 +0800

    add github workflow

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-27 11:42:34
+ * @LastEditTime: 2022-01-27 11:44:30
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /end-to-end-github-actions/index.js
+ */
+var moment = require('moment');
+var date = moment().format('YYYY-MM-DD HH:MM');
+
+console.log(date);
